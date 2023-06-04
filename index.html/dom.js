cab3e830607eebdb90f79a58b064830a919c6302
pnk-85 <pnkghogare27@gmail.com>
2023-06-04 12:13:52 +0530

    update newcode

--- index.html/dom.js
+++ index.html/dom.js
@@ -21,13 +21,35 @@ header.style.borderBottom='solid 3px #000';
 
 // getElementby class name//
 
-var items = document.getElementsByClassName('list-group-item'); 
-console.log('items');
-console.log('items[1]');
-items[1].textContent = 'hello';
-items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'yellow';
-items[3].style.backgroundColor = 'green';
-items[3].style.fontWeight = 'bold';
-items[0].style.fontWeight = 'bold';
-items[2].style.fontWeight = 'bold';
+// var items = document.getElementsByClassName('list-group-item'); 
+// console.log('items');
+// console.log('items[1]');
+// items[1].textContent = 'hello';
+// items[1].style.fontWeight = 'bold';
+// items[1].style.backgroundColor = 'yellow';
+// items[3].style.backgroundColor = 'green';
+// items[3].style.fontWeight = 'bold';
+// items[0].style.fontWeight = 'bold';
+// items[2].style.fontWeight = 'bold';
+
+// for (var i=0; i<items.length; i++){
+//     items[i].style.backgroundColor='grey';
+// }
+
+// getElementby tagName
+
+var items = document.getElementsByClassName('li'); 
+console.log('li');
+console.log('li[1]');
+li[1].textContent = 'hello';
+li[1].style.fontWeight = 'bold';
+li[1].style.backgroundColor = 'yellow';
+li[3].style.backgroundColor = 'green';
+li[3].style.fontWeight = 'bold';
+li[0].style.fontWeight = 'bold';
+li[2].style.fontWeight = 'bold';
+
+for(var i=0; i<li.length; i++){
+    li[i].style.backgroundColor = 'grey';
+}
+
