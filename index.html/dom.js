6979d1b4024fc481ee0e963fbc0edcca69d9efc7
pnk-85 <pnkghogare27@gmail.com>
2023-06-04 13:13:42 +0530

    using QuerySelector

--- index.html/dom.js
+++ index.html/dom.js
@@ -38,18 +38,46 @@ header.style.borderBottom='solid 3px #000';
 
 // getElementby tagName
 
-var items = document.getElementsByClassName('li'); 
-console.log('li');
-console.log('li[1]');
-li[1].textContent = 'hello';
-li[1].style.fontWeight = 'bold';
-li[1].style.backgroundColor = 'yellow';
-li[3].style.backgroundColor = 'green';
-li[3].style.fontWeight = 'bold';
-li[0].style.fontWeight = 'bold';
-li[2].style.fontWeight = 'bold';
+// var items = document.getElementsByClassName('li'); 
+// console.log('li');
+// console.log('li[1]');
+// li[1].textContent = 'hello';
+// li[1].style.fontWeight = 'bold';
+// li[1].style.backgroundColor = 'yellow';
+// li[3].style.backgroundColor = 'green';
+// li[3].style.fontWeight = 'bold';
+// li[0].style.fontWeight = 'bold';
+// li[2].style.fontWeight = 'bold';
 
-for(var i=0; i<li.length; i++){
-    li[i].style.backgroundColor = 'grey';
+// for(var i=0; i<li.length; i++){
+//     li[i].style.backgroundColor = 'grey';
+// }
+
+// querySelrctor
+
+var header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px #ccc';
+
+ var input = document.querySelector('input');
+input.value = 'Hello World';
+
+var submit = document.querySelector('input[type="submit"]');
+submit.value = "SEND";
+
+var seconditem = document.querySelector('.list-group-item:nth-child(2)');
+seconditem.style.color='green';
+
+var lastitem = document.querySelector('.list-group-item:last-child');
+lastitem.style.color = 'red';
+
+// querySelectorAll
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
+
+for(var i=0; i<odd.length; i++){
+    odd[i].style.backgroundColor='green';
 }
-
+for(var i=0; i<even.length; i++){
+    even[i].style.backgroundColor='orange';
+}
