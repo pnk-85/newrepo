96eb9da119e33b0d3cb7b9da907b0af634e26809
pnk-85 <pnkghogare27@gmail.com>
2023-06-04 12:00:57 +0530

    update code

--- index.html/dom.js
+++ index.html/dom.js
@@ -18,3 +18,16 @@ var header = document.getElementById('main-header');
 console.log(headerTitle);
 
 header.style.borderBottom='solid 3px #000';
+
+// getElementby class name//
+
+var items = document.getElementsByClassName('list-group-item'); 
+console.log('items');
+console.log('items[1]');
+items[1].textContent = 'hello';
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor = 'yellow';
+items[3].style.backgroundColor = 'green';
+items[3].style.fontWeight = 'bold';
+items[0].style.fontWeight = 'bold';
+items[2].style.fontWeight = 'bold';
